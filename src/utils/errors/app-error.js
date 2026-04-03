4243c36c05f54f1efe4d74e9e97f55625517b28a
aryanraj0611 <aryanraj01010@gmail.com>
2026-04-03 23:51:41 +0530

    added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,16 @@
+class AppError extends Error{
+    constructor(
+        name,
+        message,
+        explanantion,
+        statusCode
+    ){
+        super();
+        this.name = name,
+        this.message = message,
+        this.explanantion = explanantion,
+        this.statusCode = statusCode
+    }
+}
+
+module.exports = AppError;
---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,19 @@
+const { sup } = require('framer-motion/client');
+const {StatusCodes} = require('http-status-codes');
+
+class ServiceError extends Error{
+    constructor(
+        message= 'Something went wrong', 
+        explanantion= 'Service layer error', 
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ){
+        super();
+        this.name = 'ServiceError',
+        this.message = message,
+        this.explanantion = explanantion,
+        this.statusCode = statusCode;
+        
+    }
+}
+
+module.exports = ServiceError;
